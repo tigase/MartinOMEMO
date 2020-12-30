d7a32ee1f5558b496543c9980f668a9b7c0d4c6b
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2020-12-30 11:02:55 +0100

    Adjusted supported iOS and macOS versions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "TigaseSwiftOMEMO",
-    platforms: [.iOS(.v11), .macOS(.v10_14)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
