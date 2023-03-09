3c162154d646aa258c9a86c0a07655a536e55a94
Andrzej Wójcik <andrzej.wojcik@tigase.net>
2023-03-09 17:50:01 +0100

    Bumped version of Martin

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/tigase/Martin", from: "3.2.3"),
+        .package(url: "https://github.com/tigase/Martin", from: "3.2.4"),
         .package(url: "https://github.com/tigase/libsignal", from: "1.0.0")
     ],
     targets: [
